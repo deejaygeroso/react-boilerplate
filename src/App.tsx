9e0a79a11283f655babc1708198b66eea7c37144
deejay <donaldg@codev.com>
2019-12-10 18:11:45 -0700

    Single quote rule for react components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import About from './routes/About';
 import Home from './routes/Home';
@@ -7,33 +7,24 @@ import Topics from './routes/Topics';
 
 const App = (): ReactElement => {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/topics">
-            <Topics />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+        <li>
+          <Link to='/topics'>Topics</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path='/about' component={About} />
+        <Route path='/topics' component={Topics} />
+        <Route path='/' component={Home} />
+      </Switch>
+    </div>
   );
 };
 
