453d028ff0c4c3b76d65bd88d104e263691b565c
Deejay Geroso <deejaygeroso@gmail.com>
2023-02-24 10:08:34 +0800

    Update: Prettier write

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,19 +1,19 @@
-import React, { ReactElement } from "react";
-import { Link } from "react-router-dom";
+import React, { ReactElement } from 'react'
+import { Link } from 'react-router-dom'
 
 const Navigation = (): ReactElement => {
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to='/'>Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to='/about'>About</Link>
         </li>
       </ul>
     </nav>
-  );
-};
+  )
+}
 
-export default Navigation;
+export default Navigation
