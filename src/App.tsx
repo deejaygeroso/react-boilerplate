72601791ff09c4f9fe0e2319237064aad04f684e
Deejay Geroso <deejaygeroso@gmail.com>
2023-02-24 08:58:41 +0800

    Upgrade: Bump up React from v17 to v18

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React, { ReactElement } from 'react'
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import About from './modules/About'
 import Home from './modules/Home'
 import './common/styles/app.css'
 
 const App = (): ReactElement => {
-  return (
-    <Router>
-      <Switch>
-        <Route path='/about'>
-          <About />
-        </Route>
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-  )
+  const router = createBrowserRouter([
+    {
+      element: <Home />,
+      path: '/',
+    },
+    {
+      element: <About />,
+      path: 'about',
+    },
+  ])
+
+  return <RouterProvider router={router} />
 }
 
 export default App
