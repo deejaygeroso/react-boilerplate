31c89a16e30aab6139afac5afcc1cdc29976d93f
Deejay Geroso <deejaygeroso@gmail.com>
2023-02-24 09:38:40 +0800

    New: Configured testing environment

--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -1,11 +1,12 @@
 import React, { FunctionComponent, ReactElement } from 'react'
+
 import { Navigation } from '../common/components'
 
 const Home: FunctionComponent = (): ReactElement => {
   return (
     <div>
       <Navigation />
-      <h2>Home</h2>
+      <h2>HomePage</h2>
     </div>
   )
 }
